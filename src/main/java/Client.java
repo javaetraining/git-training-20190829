3c336ff459bd7873a6fb9abb93f9bba78d73df5e
Mahesh Gurrapu <mahesh@javaetraining.com>
2019-08-30 09:54:26 +0530

    adde by user3

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,8 +8,8 @@ public class Client {
 
 	public static void main(String[] args) {
 
-		System.out.println("Added from the eclipse user2");
-		System.out.println("Added by user2");
+		System.out.println("Added from the eclipse user2 &user3.");
+		System.out.println("Added by user2 & user3.");
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		StudentService studentService = context.getBean("studentService", StudentService.class);
 		
@@ -18,7 +18,7 @@ public class Client {
 		
 		System.out.println(studentService.saveStudent(student));
 		((ClassPathXmlApplicationContext) context).close();
-		System.out.println("Added by user3");
+		System.out.println("Added by user3.");
 
 	}
 
